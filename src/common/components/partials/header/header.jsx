126d7400248a888dfc1102835c2d3fd8503b492e
Klayton Faria <klayton.souza@gmail.com>
2016-12-27 19:46:37 -0200

    fix: change header style load location

--- src/common/components/partials/header/header.jsx
+++ src/common/components/partials/header/header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-require('./header.scss');
-
 const Header = () => (
   <header />
 );
--- src/common/components/partials/header/index.jsx
+++ src/common/components/partials/header/index.jsx
@@ -1,3 +1,5 @@
 import Header from './header';
+import "./header.scss";
+
 
 export default Header;
